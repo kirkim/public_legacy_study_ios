e071812718e2d4ef584be292e405fb4f9473218a
kim kirim <kirim6317@anipen.com>
2022-10-26 15:16:24 +0900

    insta API

---
+++ PracticeAPI/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  PracticeAPI
+//
+//  Created by 김기림 on 2022/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: "sample1")
+    }
+
+
+    @IBAction func handleShareInsta(_ sender: Any) {
+        if let storiesUrl = URL(string: "instagram-stories://share") {
+            if UIApplication.shared.canOpenURL(storiesUrl) {
+                // 위의 sharingImageView의 image를 image에 저장
+                guard let image = imageView.image else { return }
+                // 지원되는 형식에는 JPG,PNG 가 있다.
+                guard let imageData = image.pngData() else { return }
+                let pasteboardItems: [String: Any] = [
+                    "com.instagram.sharedSticker.stickerImage": imageData,
+                    // 배경 값 : 두 값이 다르면 그래디언트를 생성
+                    "com.instagram.sharedSticker.backgroundTopColor": "#636e72",
+                    "com.instagram.sharedSticker.backgroundBottomColor": "#b2bec3"
+                ]
+                let pasteboardOptions = [
+                    UIPasteboard.OptionsKey.expirationDate: Date().addingTimeInterval(300)
+                ]
+                UIPasteboard.general.setItems([pasteboardItems], options: pasteboardOptions)
+                UIApplication.shared.open(storiesUrl, options: [:], completionHandler: nil)
+            } else {
+                print("User doesn't have instagram on their device.")
+            }
+        }
+    }
+}
+
