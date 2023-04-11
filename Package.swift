d7591837c08cd115fafdc170a55cf0ee64ff82fb
kim kirim <kirim6317@anipen.com>
2023-04-11 14:29:04 +0900

    aa

--- Package.swift
+++ Package.swift
@@ -4,25 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "TestSegmentation",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TestSegmentation",
-            targets: ["TestSegmentation"]),
+    name: "SegmentationPerson",
+    platforms: [
+      .macOS(.v10_15), .iOS(.v14), .tvOS(.v14)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+      .library(
+          name: "SegmentationPerson",
+          targets: ["SegmentationPerson"]
+      )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TestSegmentation",
-            dependencies: []),
-        .testTarget(
-            name: "TestSegmentationTests",
-            dependencies: ["TestSegmentation"]),
+      .binaryTarget(
+          name: "SegmentationPerson",
+          path: "./Sources/SegmentationPerson.xcframework"
+      )
     ]
 )
