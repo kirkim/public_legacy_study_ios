646505158de518afa8679ed47be545dadcc38d27
kim kirim <kirim6317@anipen.com>
2023-04-13 13:15:53 +0900

    ㅁ

--- Sources/PracticePackage/PracticePackage.swift
+++ Sources/PracticePackage/PracticePackage.swift
@@ -4,7 +4,7 @@ public struct PracticePackage {
     public init() {
     }
     
-    // 문자열을 출력해주는 메서드
+    // 문자열을 출력해주는 메서드ㄴ
     // "Hello! I'm in Practice Package."가 출력
     public func printHello() {
         print("Hello! I'm in Practice Package.")
